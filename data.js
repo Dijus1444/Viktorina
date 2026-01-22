8dabfe54236b05054d9745a9d634e24108f38007
zkkkol <j.kolesova1127@gmail.com>
2026-01-22 12:44:24 +0200

    Viktorina

---
+++ data.js
@@ -0,0 +1,182 @@
+export const questions = [
+  {
+    id: 1,
+    question:
+      "Kodėl po Antrojo pasaulinio karo Europos šalys pradėjo kurti bendras organizacijas, kurios vėliau tapo ES pagrindu?",
+    answers: [
+      { text: "Siekiant ekonominio atsigavimo ir ilgalaikės taikos", isCorrect: true },
+      { text: "Norint sukurti bendrą kariuomenę prieš kitas šalis", isCorrect: false },
+      { text: "Kad visos šalys turėtų vieningą politinę sistemą", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "Kuri iš šių šalių NĖRA Europos Sąjungos narė?",
+    answers: [
+      { text: "Italija", isCorrect: false },
+      { text: "Ispanija", isCorrect: false },
+      { text: "Norvegija", isCorrect: true },
+      { text: "Portugalija", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "Kuri ES suteikiama galimybė labiausiai keičia žmonių kasdienį gyvenimą?",
+    answers: [
+      {
+        text: "Galimybė laisvai gyventi, dirbti ir studijuoti kitose ES šalyse",
+        isCorrect: true,
+      },
+      {
+        text: "Tai, kad ES priima sprendimus už nacionalines vyriausybes",
+        isCorrect: false,
+      },
+      {
+        text: "Bendra ES valiuta, naudojama daugelyje šalių",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question:
+      "Kuris miestas laikomas pagrindine Europos Sąjungos politine sostine?",
+    answers: [
+      { text: "Briuselis", isCorrect: true },
+      { text: "Strasbūras", isCorrect: false },
+      { text: "Liuksemburgas", isCorrect: false },
+      { text: "Frankfurtas", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    question: "Kokia yra pagrindinė „Erasmus+“ programos paskirtis?",
+    answers: [
+      { text: "Studentų ir jaunimo mainai", isCorrect: true },
+      { text: "Švietimo ir mokymosi galimybių plėtra", isCorrect: false },
+      { text: "Studijos, praktika ir patirtys užsienyje", isCorrect: false },
+      { text: "Padėti šalims pagerinti turizmą", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    question: "Kiek žvaigždžių yra Europos Sąjungos vėliavoje?",
+    answers: [
+      { text: "12", isCorrect: true },
+      { text: "Tiek, kiek ES šalių", isCorrect: false },
+      { text: "27", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    question: "Ką simbolizuoja ES vėliavos žvaigždės?",
+    answers: [
+      { text: "ES šalių skaičių", isCorrect: false },
+      { text: "Vienybę, solidarumą ir tobulumą", isCorrect: true },
+      { text: "Europos istorinius regionus", isCorrect: false },
+    ],
+  },
+  {
+    id: 8,
+    question: "Koks yra pagrindinis euro valiutos tikslas?",
+    answers: [
+      { text: "Palengvinti keliones", isCorrect: false },
+      { text: "Sustiprinti Europos tapatybę", isCorrect: false },
+      { text: "Supaprastinti prekybą ir ekonomiką", isCorrect: true },
+      { text: "Kontroliuoti infliaciją visame pasaulyje", isCorrect: false },
+    ],
+  },
+  {
+    id: 9,
+    question: "Kuri iš šių sričių dažniausiai finansuojama ES lėšomis?",
+    answers: [
+      { text: "Keliai ir infrastruktūra", isCorrect: false },
+      { text: "Švietimas ir mokslas", isCorrect: false },
+      { text: "Regionų vystymas", isCorrect: false },
+      { text: "Visos išvardytos", isCorrect: true },
+    ],
+  },
+  {
+    id: 10,
+    question: "Ką reiškia Šengeno erdvė?",
+    answers: [
+      { text: "Bendrą valiutą", isCorrect: false },
+      {
+        text: "Laisvą asmenų judėjimą be sienų kontrolės",
+        isCorrect: true,
+      },
+      { text: "Bendrą gynybos sistemą", isCorrect: false },
+    ],
+  },
+  {
+    id: 11,
+    question: "Kuri šalis NAUDOJA eurą, bet NĖRA Europos SąjungOS narė?",
+    answers: [
+      { text: "Lenkija", isCorrect: false },
+      { text: "Norvegija", isCorrect: false },
+      { text: "Šveicarija", isCorrect: false },
+      { text: "Vatikanas", isCorrect: true },
+    ],
+  },
+  {
+    id: 12,
+    question: "Koks yra Europos Parlamento vaidmuo?",
+    answers: [
+      {
+        text: "Atstovauti ES piliečius ir priimti teisės aktus",
+        isCorrect: true,
+      },
+      {
+        text: "Vykdyti ES įstatymus valstybėse narėse",
+        isCorrect: false,
+      },
+      {
+        text: "Atstovauti tik nacionalinėms vyriausybėms",
+        isCorrect: false,
+      },
+      { text: "Skirti ES teisėjus", isCorrect: false },
+    ],
+  },
+  {
+    id: 13,
+    question:
+      "Kaip vadinasi ES programa, skirta moksliniams tyrimams ir inovacijoms?",
+    answers: [
+      { text: "Erasmus+", isCorrect: false },
+      { text: "Horizon Europe", isCorrect: true },
+      { text: "LIFE", isCorrect: false },
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "Ar valstybė gali pati nuspręsti pasitraukti iš Europos Sąjungos?",
+    answers: [
+      { text: "Taip", isCorrect: true },
+      { text: "Ne", isCorrect: false },
+    ],
+  },
+  {
+    id: 15,
+    question:
+      "Kuris teiginys apie Europos Sąjungą yra TEISINGAS?",
+    answers: [
+      { text: "ES sprendžia tik ekonominius klausimus", isCorrect: false },
+      {
+        text:
+          "ES valstybės narės perduoda dalį savo sprendimų ES institucijoms",
+        isCorrect: true,
+      },
+      {
+        text:
+          "ES gali savarankiškai panaikinti nacionalinius įstatymus be valstybių sutikimo",
+        isCorrect: false,
+      },
+      {
+        text: "ES turi vieną bendrą vyriausybę visoms šalims",
+        isCorrect: false,
+      },
+    ],
+  },
+];
